6d7db67393399a06eb2e7a036153d23d5bd7d719
Alex Chernov <nak4442@gmail.com>
2025-03-18 23:39:44 +0300

    Команда перезапуска

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,10 @@
 from .start_handler import StartHandler
 from .game_handler import GameHandler
+from .restart_handler import RestartHandler
 
 
 __all__ = [
     "StartHandler",
-    "GameHandler"
+    "GameHandler",
+    "RestartHandler"
 ]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from services import GameService
 from routers import MainRouter
-from handlers import StartHandler, GameHandler
+from handlers import *
 from keyboards import GameKeyboard
 from utils import load_config
 import asyncio
@@ -18,7 +18,12 @@ async def main():
     game_keyboard = GameKeyboard()
     start_handler = StartHandler()
     game_handler = GameHandler(game_service, game_keyboard)
-    main_router = MainRouter(start_handler, game_handler)
+    restart_handler = RestartHandler(game_service)
+    main_router = MainRouter(
+        start_handler, 
+        game_handler,
+        restart_handler
+    )
     main_router.setup_routes()
     dp.include_router(main_router)
     await dp.start_polling(bot)
